ff00fc87d3454e663220999bb20b20fe165ad6be
DANNY OXBY <b9017792@hallam.shu.ac.uk>
2023-05-12 10:41:00 +0100

    added a unit testing package for the validation methods

---
+++ ArdonaghProjectTests/Services/ValidationServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingTest.Services.Tests
+{
+    [TestClass()]
+    public class ValidationServiceTests
+    {
+        #region name tests
+        [TestMethod()]
+        public void NameValidationCorrectInputTest()
+        {
+            Assert.IsTrue(ValidationService.NameValidation("Henry Cavel"), "NameValidationCorrectInputTest Returned False instead of True");
+        }
+        [TestMethod()]
+        public void NameValidationInCorrectInputTest()
+        {
+            string lengthtest = "123456789012345678901234567890123456789012345678901234567890";
+
+            Assert.IsTrue(lengthtest.Length > 50, "Not testing the correct length of input");
+            Assert.IsFalse(ValidationService.NameValidation(lengthtest), "NameValidationInCorrectInputTest Returned True instead of False");
+        }
+        [TestMethod()]
+        public void NameValidationNullInputTest()
+        {
+            Assert.IsFalse(ValidationService.NameValidation(null), "NameValidationNullInputTest Returned True instead of False");
+        }
+        #endregion
+
+        #region age tests
+        [TestMethod()]
+        public void AgeValidationCorrectInputTest()
+        {
+            int agetest = 48;
+
+            Assert.IsTrue(agetest > 0 && agetest <= 110, "Testing value is out of intended scope");
+            Assert.IsTrue(ValidationService.AgeValidation(agetest), "AgeValidationCorrectInputTest Returned False instead of True");
+
+            int boundryagetest = 110;
+            Assert.IsTrue(agetest > 0 && agetest <= 110, "Testing boundry value is out of intended scope");
+            Assert.IsTrue(ValidationService.AgeValidation(boundryagetest), "AgeValidationCorrectInputTest boundry Returned False instead of True");
+
+        }
+
+        [TestMethod()]
+        public void AgeValidationInCorrectInputTest()
+        {
+            int agetest = -1;
+
+            Assert.IsTrue(agetest < 0, "Testing value is out of intended scope");
+            Assert.IsFalse(ValidationService.AgeValidation(agetest), "AgeValidationCorrectInputTest Returned True instead of False");
+
+            int boundryagetest = 111;
+            Assert.IsTrue(boundryagetest > 110, "Testing boundry value is out of intended scope");
+            Assert.IsFalse(ValidationService.AgeValidation(boundryagetest), "AgeValidationCorrectInputTest boundry Returned True instead of False");
+
+        }
+        #endregion
+
+        #region postcode tests
+        [TestMethod()]
+        public void PostcodeValidationCorrectInputTest() {
+            Assert.IsTrue(ValidationService.PostCodeValidation("AS21 4FG"), "PostCodeVaildation Returned False instead of True");
+        }
+
+        [TestMethod()]
+        public void PostcodeValidationInCorrectInputTest() { 
+            Assert.IsFalse(ValidationService.PostCodeValidation("ASGB UFG"), "PostCodeVaildation Returned True instead of False");
+        }
+        #endregion
+    }
+}
--- ProgrammingTest/Services/ValidationService.cs
+++ ProgrammingTest/Services/ValidationService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProgrammingTest.Services
 {
-    internal class ValidationService
+    public static class ValidationService
     {
 
         /*
@@ -15,5 +15,37 @@ namespace ProgrammingTest.Services
         public string PostCode { get; set; } // Must contain numbers and characters
         public double Height { get; set; } // 0 to 2.50. Only 2 decimal places
          */
+
+        public static bool NameValidation(string nameinput)
+        {
+            if(string.IsNullOrEmpty(nameinput)) return false; // input has value
+            else if (nameinput.Length > 50) return false; //input meets criteria
+            else return true; //return condition
+        }
+
+        public static bool AgeValidation(int ageinput)
+        {
+            if (ageinput < 0 || ageinput > 110) return false; //input meets criteria
+            else return true; //return condition
+        }
+
+        public static bool PostCodeValidation(string postcodeinput)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(postcodeinput)) return false; // input has value
+                else
+                {
+                    if (postcodeinput.Any(Char.IsLetter)) //is there any letter
+                        if (postcodeinput.Any(Char.IsDigit)) return true; //is there any number
+                        else return false;
+                    else return false;
+                }
+            } catch (Exception ex) 
+            { 
+                string debugmsg = ex.Message;
+                return false; 
+            }
+        }
     }
 }
